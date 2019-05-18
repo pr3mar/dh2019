f072af7c81be6c74e9cbf474b03fdcf95ef49293
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-05-18 19:23:01 +0200

    fixing format

--- manager/manager.py
+++ manager/manager.py
@@ -3,26 +3,29 @@ import json
 import flask
 from flask import request, jsonify
 from scheduler import optimize
+from utils import getRegionIdByName
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-regions = {
-    "San Francisco": {
+regions = [
+    {
+        "region": "San Francisco",
         "demand": [2, 3, 4, 5, 9, 6, 11],
-        "nodes": ["1", "2", "3", "4"],
+        "pods": 5,
     },
-    "London": {
+    {
+        "region": "London",
         "demand": [5, 3, 8, 5, 5, 5, 5],
-        "nodes": ["5", "6", "7"]
+        "pods": 7
     },
-
-    "Tokyo": {
+    {
+        "region": "Tokyo",
         "demand": [5, 3, 8, 5, 3, 2, 1, 1],
-        "nodes": ["9", "10", "11"]
+        "pods": 11
     }
-}
+]
 
 
 @app.route('/', methods=['GET'])
@@ -36,11 +39,12 @@ def demand_update():
 
     region = data["region"]
     value = data["value"]
-
-    regions[region]["demand"].append(value)
-    optimized = optimize([regions[region]["demand"][-1] for region in regions], 30, debug=True)
-
-    return jsonify({"message": "demand added to {}".format(region)})
-
+    try:
+        regionId = getRegionIdByName(regions, region)
+        regions[regionId]["demand"].append(value)
+        optimized = optimize([region["demand"][-1] for region in regions], 30, debug=True)
+        return jsonify({"message": "demand added to {}".format(region)})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 app.run()
