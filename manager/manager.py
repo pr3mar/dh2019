77e1dc4097201ae9361566a7d6619ec8c8e17d91
svigelj <aljaz.svigelj@gmail.com>
2019-05-19 01:08:24 +0200

    Working locally

--- manager/manager.py
+++ manager/manager.py
@@ -4,28 +4,49 @@ import flask
 from flask import request, jsonify
 from scheduler import optimize
 from utils import getRegionIdByName
+from kubernetes import client, config
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+config.load_incluster_config()
+extensions_v1beta1 = client.ExtensionsV1beta1Api()
 
 regions = [
     {
-        "region": "San Francisco",
-        "demand": [2, 3, 4, 5, 9, 6, 11],
+        "region": "Los Angeles",
+        "name": "eco-miner-la",
+        "demand": [2, 3, 4, 5, 9, 6],
         "pods": 5,
     },
     {
         "region": "London",
-        "demand": [5, 3, 8, 5, 5, 5, 5],
+        "name": "eco-miner-lo",
+        "demand": [5, 3, 8, 5, 5, 5],
         "pods": 7
     },
     {
         "region": "Tokyo",
-        "demand": [5, 3, 8, 5, 3, 2, 1, 1],
+        "name": "eco-miner-to",
+        "demand": [5, 3, 8, 5, 3, 2],
         "pods": 11
     }
 ]
+
+
+def update_deployment(api_instance, deployment, name, n_replicas):
+    # Update container image
+    deployment.spec.replicas = n_replicas
+    # Update the deployment
+    api_response = api_instance.patch_namespaced_deployment(
+        name=name,
+        namespace="default",
+        body=deployment)
+    print("Deployment updated. status={}, replica_count={}".format(api_response.status, n_replicas))
+
+
+def get_deployment(api_instance, name):
+    return api_instance.read_namespaced_deployment(name, namespace="default")
 
 
 @app.route('/', methods=['GET'])
@@ -39,12 +60,22 @@ def demand_update():
 
     region = data["region"]
     value = data["value"]
+
     try:
-        regionId = getRegionIdByName(regions, region)
-        regions[regionId]["demand"].append(value)
+        region_id = getRegionIdByName(regions, region)
+        regions[region_id]["demand"] = regions[region_id]["demand"][1:]
+        regions[region_id]["demand"].append(value)
+
         optimized = optimize([region["demand"][-1] for region in regions], 30, debug=True)
+
+        for i, region in enumerate(regions):
+            region["pods"] = optimized[i]
+            deployment = get_deployment(extensions_v1beta1, region["name"])
+            update_deployment(extensions_v1beta1, deployment, region["name"], optimized[i])
+
         return jsonify({"message": "demand added to {}".format(region)})
     except Exception as e:
         return jsonify({"error": str(e)})
+
 
 app.run(host='0.0.0.0')
