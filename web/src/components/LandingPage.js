a8ceb82e685dab7a2ad57a8a88ba4cf590fa01f7
svigelj <aljaz.svigelj@gmail.com>
2019-06-06 13:07:56 +0200

    https

--- web/src/components/LandingPage.js
+++ web/src/components/LandingPage.js
@@ -35,7 +35,7 @@ class LandingPage extends Component {
   }
 
   fetchApiData = () => {
-    axios.get("http://88.200.23.39")
+    axios.get("https://88.200.23.39")
       .then(response => {
         let apiData = response.data;
         for (let i = 0; i < apiData.length; i++) {
